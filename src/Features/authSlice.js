7817404a958c7b4b4769780354b829f618edbdef
loayelden <loayeldin123@gmail.com>
2025-06-09 20:17:31 +0300

    fixed bug in build

--- src/Features/authSlice.js
+++ src/Features/authSlice.js
@@ -51,5 +51,3 @@ export const authSlice = createSlice({
     
     }
 })
-
-// Ahmedahmed1!
--- src/Store/store.js
+++ src/Store/store.js
@@ -1,10 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { authSlice } from "../features/authSlice";
-import { productsSlice } from "../features/productsSlice";
-import { productDetailsSlice } from "../features/productDetailsSlice";
-import { categoriesSlice } from "../features/categoriesSlice";
-import { wishlistSlice } from "../features/wishListSlice";
-import { CartSlice } from "../features/CartSlice";
+import { authSlice } from "../Features/authSlice";
+import { productsSlice } from "../Features/productsSlice";
+import { productDetailsSlice } from "../Features/productDetailsSlice";
+import { categoriesSlice } from "../Features/categoriesSlice";
+import { wishlistSlice } from "../Features/wishListSlice";
+import { CartSlice } from "../Features/CartSlice";
 
 export default configureStore({
   reducer: {
